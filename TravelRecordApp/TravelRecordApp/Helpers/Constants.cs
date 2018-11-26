36a18ed30de59570012ba34c17f915e517d7e68a
frahm <frahmcl@gmail.com>
2018-11-26 11:32:13 -0600

    First Commit

---
+++ TravelRecordApp/TravelRecordApp/Helpers/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TravelRecordApp.Helpers
+{
+    class Constants
+    {
+        public const string VENUE_SEARCH = "https://api.foursquare.com/v2/venues/search?ll={0},{1}&client_id={2}&client_secret={3}&v={4}";
+        public const string CLIENT_ID = "4X0JXCGMMN0DAGRBKMFJAIF5MFQW4DFU5Q0JGFZSK0Q41EEO";
+        public const string CLIENT_SECRET = "RO4DCB3URRZR45TWVODEZINBGZ1YAECTUIIAONRTCSM552GK";
+    }
+}
---
+++ TravelRecordApp/TravelRecordApp/HistoryPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using TravelRecordApp.Model;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TravelRecordApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class HistoryPage : ContentPage
+	{
+		public HistoryPage ()
+		{
+			InitializeComponent ();
+		}
+
+        //check for updates to database everytime navigateing to history page
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            //variable only exists in this block, once leaves this block, it disposes of the connection, just like conn.Close();
+            using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            {
+                conn.CreateTable<Post>();
+                var posts = conn.Table<Post>().ToList();
+                postListView.ItemsSource = posts;
+            }
+        }
+    }
+}
